fc7b12aa5c412b7cba5a1136f79d57d72f1db595
joey2031 <36198917+joey2031@users.noreply.github.com>
2018-02-14 17:12:51 -0500

    Update main.c

--- HelloWorld (C)/main.c
+++ HelloWorld (C)/main.c
@@ -1,21 +1,14 @@
-/* 
- * File:   main.c
- * Author: Joey
- *
- * Created on February 13, 2018, 7:25 PM
- */
 
 #include <stdio.h> // saying to include functions/ tools to be able to use to build our program
 #include <stdlib.h>
 
-/*
- * 
- */
+
+
 int main(int argc, char** argv) { // like the main method in java it is called a
                                   //function, same thing as methods
      printf("Hello, World!\n"); // print statement. \n will print the next word 
                                //on a new line.
-     printf ("joey\n"); // since nothing is under this print statement it will 
+     printf ("bob\n"); // since nothing is under this print statement it will 
                        //print a new line of white space.
      
      printf ("tab1\t"); // will tab the next word below.
