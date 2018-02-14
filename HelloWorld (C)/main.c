d2a8deb39a720a635dc8ca8e46de67ccd0f4798a
joey2031 <36198917+joey2031@users.noreply.github.com>
2018-02-13 19:55:26 -0500

    Add files via upload

---
+++ HelloWorld (C)/main.c
@@ -0,0 +1,30 @@
+/* 
+ * File:   main.c
+ * Author: Joey
+ *
+ * Created on February 13, 2018, 7:25 PM
+ */
+
+#include <stdio.h> // saying to include functions/ tools to be able to use to build our program
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) { // like the main method in java it is called a
+                                  //function, same thing as methods
+     printf("Hello, World!\n"); // print statement. \n will print the next word 
+                               //on a new line.
+     printf ("joey\n"); // since nothing is under this print statement it will 
+                       //print a new line of white space.
+     
+     printf ("tab1\t"); // will tab the next word below.
+     printf ("tab2"); 
+     
+     
+     
+     return 0; // if your program gets to this point and it returns 0. it means it has run successfully
+
+// this is a comment.
+}
+
